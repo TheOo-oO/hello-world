7bcc924a828f8e5e2ddece5d5c46d14c6381e240
TheOo-oO <47270189+TheOo-oO@users.noreply.github.com>
2020-02-08 20:16:23 +1100

    Create sddtest.py

---
+++ sddtest.py
@@ -0,0 +1,14 @@
+#Case 1: SDD question 
+
+y = 8 
+x = y # x = 8 
+y = x  # y = 8 
+print(y, x )
+
+#Case 2: Switching variables 
+x = 5 
+x = 5 
+y = 8  
+y = x # y = 5 
+x = y # x = 5 
+print(x, y )
